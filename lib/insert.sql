63b4c6a2815effa214dcdb8d51d6abf1b5fcd0e9
ElvineJoy <elvinkuiyungest@gmail.com>
2024-07-21 16:58:12 +0300

    Lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Mr.Chocolate',3, 'M', 'Brown', 'Sanguine', 1);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Rowdy',10, 'F', 'Black', 'Melancholy', 0);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Tabitha',5, 'F', 'Polar', 'Sanguine', 1);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Sergeant Brown',15, 'M', 'White', 'Choleric', 1);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Melissa',2, 'F', 'White', 'Phlegmatic', 1);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Grinch',9, 'M', 'Green', 'Choleric', 0);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES ('Wendy',6, 'F', 'Brown', 'Phlegmatic', 1);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES (NULL,1, 'M', 'White', 'NULL', 1);
+
+-- sanguine, choleric, melancholic, phlegmatic
